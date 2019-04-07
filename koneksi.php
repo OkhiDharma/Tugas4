f43e3888e619dad7f7fdcfc39a8faaff14505628
OkhiDharma <49054054+OkhiDharma@users.noreply.github.com>
2019-04-07 15:49:25 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,6 @@
+<?php
+    $kon = mysqli_connect("localhost", "id9203253_akademik","22101994semarang","id9203253_tugas4");
+    if (mysqli_connect_errno()) {
+        echo "Koneksi database gagal : " .mysqli_connect_error();
+    }
+?>
